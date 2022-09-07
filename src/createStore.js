9005215f4babcdc0dbbd1a7630d60cf34aeab6c6
davit.ishkhanyan.k <davit.ishkhanyan.k@digitain.com>
2022-09-07 15:56:14 +0400

    my redux

--- src/createStore.js
+++ src/createStore.js
@@ -1,12 +1,11 @@
-export function createStore() {
-    const state = {};
+export function createStore(rootReducer, initialState) {
+    let state = rootReducer(initialState, {type: '__INIT__'});
     const subscribers = [];
 
-
     return {
-        // action === {type: 'INCREMENT'}
-        dispatch(actions) {
-
+        dispatch(action) {
+            state = rootReducer(state, action);
+            subscribers.forEach(sub => sub());
         },
         subscribe(callback) {
             subscribers.push(callback);
